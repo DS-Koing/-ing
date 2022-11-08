e6bdac4c97b0077256c6e6353c926a4f95889349
jiho-par <hkp314518@duksung.ac.kr>
2022-11-08 14:39:32 +0900

    @beans 오류 다시 해결 인텔리제이 버그 idea.spring.boot.filter.autoconfig=false 추가

--- src/main/java/com/example/ing/config/SwaggerConfiguration.java
+++ src/main/java/com/example/ing/config/SwaggerConfiguration.java
@@ -13,25 +13,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
-    @Bean
-    /*
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.springboot.api"))
-                .paths(PathSelectors.any())
-                .build();
-    }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Spring Boot Open API Test with Swagger")
-                .description("설명 부분")
-                .version("1.0.0")
-                .build();
-    }
-    */
+    @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
